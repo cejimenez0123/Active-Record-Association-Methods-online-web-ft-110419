7a5d858c4c5a365552b1861efaf2533f58673386
Christian Jimenez <christianjimenez0123@gmail.com>
2019-12-26 18:47:12 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,10 +3,11 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    self.count(:songs)#return the genre of the artist's first saved song
+    self.songs.first.genre#return the genre of the artist's first saved song
   end
 
   def song_count
+    self.count(:songs)
     #return the number of songs associated with the artist
   end
 
